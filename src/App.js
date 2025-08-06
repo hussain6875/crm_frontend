401abd44cb95a91ac12157b6629d19a2cf22383b
Hussain <hussainmhn75@gmail.com>
2025-08-06 15:20:04 +0530

    add lead detail page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Dashboard from "./pages/Dashboard";
 import Leads from "./pages/Leads";
 import Companies from "./pages/Companies";
@@ -12,19 +12,21 @@ import TicketDetails from "./pages/TicketDetails";
 import LeadDetails from "./pages/LeadDetails";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/leads" element={<Leads />} />
-        <Route path="/companies" element={<Companies />} />
-        <Route path="/deals" element={<Deals />} />
-        <Route path="/tickets" element={<Tickets />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/leads" element={<Leads />} />
+                <Route path="/companies" element={<Companies />} />
+                <Route path="/deals" element={<Deals />} />
+                <Route path="/tickets" element={<Tickets />} />
+                <Route path="/tickets/:ticketId/details" element={<TicketDetails />} />
+                <Route path="/leads/:leadId/details" element={<LeadDetails />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
