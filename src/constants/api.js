e07e4dbfdeb131b747d2a8f16ec904a00d2cc372
Suhail <suhail518650@gmail.com>
2025-09-01 12:41:20 +0530

    updated

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,10 +1,10 @@
 // Base URL for your backend API
-export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = "http://localhost:8080/api/users";
 
 // Auth Endpoints
 export const AUTH_ENDPOINTS = {
-  REGISTER: `${API_BASE_URL}/auth/register`,
-  LOGIN: `${API_BASE_URL}/auth/login`,
-  FORGOT_PASSWORD: `${API_BASE_URL}/auth/forgot-password`,
+  REGISTER: `${API_BASE_URL}/register`,
+  LOGIN: `${API_BASE_URL}/login`,
+  FORGOT_PASSWORD: `${API_BASE_URL}/forgot-password`,
 };
 
