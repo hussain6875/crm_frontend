125e621fbf30160be7f8736956baf1122a402e7c
Serin Elsa <serinelsa@gmail.com>
2025-09-02 23:57:54 +0400

    fixed errors in import

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import activityReducer from "./features/activitySlice";
 import leadsReducer from '../redux/features/leads/leadsSlice';
 import userReducer from "./userSlice";
 import fileReducer from "./features/fileSlice";
-import authReducer from "./AuthSlice"
+
 
 const store = configureStore({
   reducer: {
